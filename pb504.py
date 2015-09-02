233f37b90727e42252c0d8988fbe3f00e6428d3c
Jenny <jenny-lyon@outlook.com>
2015-09-02 18:24:11 +0200

    02/09/2015

---
+++ pb504.py
@@ -0,0 +1,47 @@
+# -------------------------------------------------------------------------------
+# Name:        pb190
+# Purpose:     project euler
+# Created:     21/08/2015
+# -------------------------------------------------------------------------------
+
+import math
+import fractions
+import time
+import sys
+
+
+class Problem():
+
+    def __init__(self, n):
+        self.n = n
+
+    @staticmethod
+    def issquare(x):
+        rac = math.sqrt(x)
+        return rac == int(rac)
+
+    def get_count(self):
+        compteur = 0
+        for i1 in range(1, self.n+1):
+            for i2 in range(1, self.n+1):
+                g12 = fractions.gcd(i1, i2)
+                for i3 in range(1, self.n+1):
+                    g23 = fractions.gcd(i2, i3)
+                    aplusc = i1 + i3
+                    for i4 in range(1, self.n+1):
+                        if self.issquare(1+(aplusc*(i2+i4)-g12-g23-fractions.gcd(i3, i4)-fractions.gcd(i4, i1))/2):
+                            compteur += 1
+        return compteur
+
+    def solve(self):
+        print(self.get_count())
+
+
+def main():
+    start = time.perf_counter()
+    u = Problem(100)
+    u.solve()
+    print('temps d execution', time.perf_counter() - start, 'sec')
+
+if __name__ == '__main__':
+    sys.exit(main())
