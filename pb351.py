48daab52b955617658da29b0dd47b7d581252b48
Jenny <jenny-lyon@outlook.com>
2015-09-07 00:18:38 +0200

    06/09/2015

--- pb351.py
+++ pb351.py
@@ -6,19 +6,19 @@
 
 import time
 import sys
+import numpy
 
 
 def phi_sieve(lim):
     limit = lim + 1
-    visited = [False] * limit
-    phi = [i for i in range(limit)]
+    phi = numpy.empty(limit, dtype='int32')
+    for i in range(limit):
+        phi[i] = i
     for i in range(2, limit):
-        if not visited[i]:
+        if phi[i] == i:
             phi[i] = i-1
             for j in range(i + i, limit, i):
                 phi[j] = phi[j]//i * (i-1)
-                visited[j] = True
-    del visited
     return phi
 
 
