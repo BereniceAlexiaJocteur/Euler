0c18aabf24b6efb97ee5e828034593b11bab7ec4
Jenny <jenny-lyon@outlook.com>
2015-09-09 22:11:23 +0200

    09/09/2015

--- pb157.py
+++ pb157.py
@@ -19,10 +19,10 @@ class Problem():
         self.pwrs5 = [5 ** k for k in range(int(math.log(2 * 10 ** self.lim, 5)) + 1)]
 
     def num_sol(self, n):
-        hamming = [a * b for a in self.pwrs2 for b in self.pwrs5 if a * b <= 2 * 10 ** n]
-        lolo = [(a, b) for a in hamming for b in hamming if
+        products = [a * b for a in self.pwrs2 for b in self.pwrs5 if a * b <= 2 * 10 ** n]
+        candidates = [(a, b) for a in products for b in products if
                 a <= b and ((b + a) * 10 ** n) % (a * b) == 0 and fractions.gcd(a, b) == 1]
-        listp = [((a + b) * 10 ** n) // (a * b) for a, b in lolo]
+        listp = [((a + b) * 10 ** n) // (a * b) for a, b in candidates]
         resu = 0
         for a in listp:
             resu += sympy.divisor_count(a)
