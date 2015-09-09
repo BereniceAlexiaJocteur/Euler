187e3fa4c7d4e8cf3cc07df4633eba579d01796b
Jenny <jenny-lyon@outlook.com>
2015-09-09 14:28:06 +0200

    09/09/2015

---
+++ pb157.py
@@ -0,0 +1,45 @@
+# -------------------------------------------------------------------------------
+# Name:        pb157
+# Purpose:     project euler
+# Created:     09/09/2015
+# -------------------------------------------------------------------------------
+
+import fractions
+import math
+import sympy
+import time
+import sys
+
+
+class Problem():
+
+    def __init__(self, n):
+        self.lim = n
+        self.pwrs2 = [2 ** k for k in range(int(math.log(2 * 10 ** self.lim, 2)) + 1)]
+        self.pwrs5 = [5 ** k for k in range(int(math.log(2 * 10 ** self.lim, 5)) + 1)]
+
+    def num_sol(self, n):
+        hamming = [a * b for a in self.pwrs2 for b in self.pwrs5 if a * b <= 2 * 10 ** n]
+        lolo = [(a, b) for a in hamming for b in hamming if
+                a <= b and ((b + a) * 10 ** n) % (a * b) == 0 and fractions.gcd(a, b) == 1]
+        listp = [((a + b) * 10 ** n) // (a * b) for a, b in lolo]
+        resu = 0
+        for a in listp:
+            resu += sympy.divisor_count(a)
+        return resu
+
+    def solve(self):
+        rsufinal = 0
+        for w in range(1, self.lim+1):
+            rsufinal += self.num_sol(w)
+        print(rsufinal)
+
+
+def main():
+    start = time.perf_counter()
+    u = Problem(9)
+    u.solve()
+    print('temps d execution', time.perf_counter() - start, 'sec')
+
+if __name__ == '__main__':
+    sys.exit(main())
