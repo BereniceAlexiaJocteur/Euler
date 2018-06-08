e7139575cc49c50766ce79733f6abfc7d4ea6517
Jenny <jenny-lyon@outlook.com>
2018-06-08 20:20:26 +0200

    08/06/2018

--- pb180.py
+++ pb180.py
@@ -1,10 +1,6 @@
-# -------------------------------------------------------------------------------
-# Name:        
-# Purpose:     
-# Created:     
-# -------------------------------------------------------------------------------
-
 import fractions
+import time
+import sys
 
 
 class Problem():
@@ -13,21 +9,50 @@ class Problem():
         self.order = order
         self.set_of_rationals = set()
         self.set_of_sums = set()
-        self.total_fraction = 0
+        self.res = 0
 
-    def init_list(self):
+    def init_set_of_rationnals(self):
         for i in range(2, self.order+1):
             for j in range(1, i):
                 self.set_of_rationals.add(fractions.Fraction(j, i))
 
-    @staticmethod
-    def test_fi_and_n(n, x, y, z):
-        list_fi = [x**(n+1)+y**(n+1)-z**(n+1), (x*y+x*z+y*z)*(x**(n-1)+y**(n-1)-z**(n-1)), x*y*z*(x**(n-2)+y**(n-2)-z*(n-2))]
-        return list_fi[0] + list_fi[1] + list_fi[3] == 0
+    def init_set_of_sums(self):
+        list_of_rationals = list(self.set_of_rationals)
+        for indice_x in range(len(list_of_rationals)):
+            ax = list_of_rationals[indice_x].numerator
+            bx = list_of_rationals[indice_x].denominator
+            for indice_y in range(indice_x, len(list_of_rationals)):
+                ay = list_of_rationals[indice_y].numerator
+                by = list_of_rationals[indice_y].denominator
+                for indice_z in range(len(list_of_rationals)):
+                    az = list_of_rationals[indice_z].numerator
+                    bz = list_of_rationals[indice_z].denominator
+                    num1 = ax*by*bz
+                    num2 = ay*bx*bz
+                    num3 = az*bx*by
+                    denom1 = bx*ay*az
+                    denom2 = by*ax*az
+                    denom3 = ax*ay*bz
+                    if num1 + num2 == num3 or num1**2 + num2**2 == num3**2 or denom1 + denom2 == denom3 or denom1**2 + denom2**2 == denom3**2:
+                        self.set_of_sums.add(list_of_rationals[indice_x] + list_of_rationals[indice_y] + list_of_rationals[indice_z])
 
-    def test(self):
-        print(self.set_of_rationals)
+    def get_res(self):
+        somme = sum(self.set_of_sums)
+        self.res = somme.numerator + somme.denominator
 
-u = Problem(35)
-u.init_list()
-u.test()
+    def solve(self):
+        self.init_set_of_rationnals()
+        self.init_set_of_sums()
+        self.get_res()
+        print(self.res)
+
+
+def main():
+    start = time.perf_counter()
+    u = Problem(35)
+    u.solve()
+    print('temps d\'exécution', time.perf_counter() - start, 'sec')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
