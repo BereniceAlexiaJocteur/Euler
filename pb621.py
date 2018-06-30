82aeefc70170bcb39099ec8d34f666624f1a4122
Jenny <jenny-lyon@outlook.com>
2018-06-30 09:31:04 +0200

    30/06/2018

--- pb621.py
+++ pb621.py
@@ -1,60 +1,81 @@
 import time
 import sys
 import math
-from sympy import sieve
+
+"""I solved this problem using http://www.personal.psu.edu/jxs23/p7.pdf and 
+http://mathworld.wolfram.com/SumofSquaresFunction.html"""
 
 
 class Problem():
 
-    def __init__(self):
-        self.primes = None
-
-    def r2(self, n):
-        produit = 1
+    @staticmethod
+    def r2(n):  # returns numbers of ways of writing n as a sum of two positive squares
+        B = 1
         while n % 2 == 0:
             n //= 2
-        for p in self.primes:
-            if n == 0:
-                break
-            expo = 0  # exposant du facteur premier
+        p = 3
+        while n > 1 and p**2 <= n:
+            coeff_de_p = 0
             while n % p == 0:
+                coeff_de_p += 1
                 n //= p
-                expo += 1
-            if p % 4 == 3 and expo % 2 == 1:
+            if coeff_de_p % 2 != 0:
                 return 0
-            elif p % 4 == 1:
-                produit *= (expo + 1)
-        return produit
+            p += 2
+            if n == 1 or p**2 > n:
+                break
+            coeff_de_p = 0
+            while n % p == 0:
+                coeff_de_p += 1
+                n //= p
+            B = B*(coeff_de_p + 1)
+            p += 2
+        if n > 1:  # n est nécessairement premier
+            if n % 4 == 3:
+                return 0
+            else:
+                B *= 2
+        return B
 
-    def r3_naif(self, n):
-        somme = 0
+    def r3_brute(self, n): # returns numbers of ways of writing n as a sum of three positive squares
+        res = 0
         for i in range(1, int(math.sqrt(n))+1):
-            somme += self.r2(n-i**2)
-        return somme
+            res += self.r2(n-i*i)
+        return res
 
-    def r3(self, n):
-        initial_n = n
+    def r3_opti(self, n):
         lamb = 0
         while n % 9 == 0:
-            n //= 9
             lamb += 1
-        print(n)
-        self.primes = sieve.primerange(3, n)
-        if n % 24 == 11:
-            return (3**lamb) * self.r3_naif(n)
-        elif n % 24 == 19:
-            return (2*3**lamb-1)*self.r3_naif(n)
-        elif n % 72 == 3 or n % 72 == 51:
-            return ((3**(lamb+1)-1)*self.r3_naif(n))//2
-        else:
-            return self.r3_naif(initial_n)
+            n //= 9
+        if lamb > 0:
+            if n % 24 == 11:
+                return 3**lamb*self.r3_opti(n)
+            if n % 24 == 19:
+                return (2*3**lamb-1)*self.r3_opti(n)
+            if n % 72 == 3 or n % 72 == 51:
+                return ((3**(lamb+1)-1)//2)*self.r3_opti(n)
+        n = n * 9**lamb
+        return self.r3_brute(n)
 
-    def g(self, n):
-        return self.r3(8*n+3)
+    def G(self, n):
+        if n % 27 == 12:
+            u = n // 27
+            return 3*self.G(3*u+1)
+        if n % 27 == 21:
+            u = n // 27
+            return 5*self.G(3*u+2)
+        if n % 81 == 3:
+            u = n // 81
+            return 4*self.G(9*u)
+        if n % 81 == 57:
+            u = n // 81
+            return 4*self.G(9*u+6)
+        else:
+            return self.r3_opti(8*n+3)
 
     def solve(self):
-        print(self.g(17526*10**9))
-
+        print(self.G(17526*10**9))
 
 
 def main():
